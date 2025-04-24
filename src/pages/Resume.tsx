1307bacaff3b619ed89d1bf5014acf7067a1ea0d
Jamerson <jamescao800@gmail.com>
2025-04-24 18:38:56 -0400

    finished the basic version of the resume section choose not to use a lib and just an iframe

--- src/pages/Resume.tsx
+++ src/pages/Resume.tsx
@@ -1,10 +1,15 @@
 export default function Resume() {
   return (
     <div className="flex flex-col items-center justify-center w-full h-full">
-      <h1 className="text-2xl font-bold">About Page</h1>
-      <p className="mt-4 text-gray-600">
-        This is the about page of the application.
-      </p>
+      <div className="flex mt-50 mb-30 w-3/5 h-screen">
+        <iframe
+          src="\resume.pdf#toolbar=0&height=100vh"
+          style={{border:"none"}}
+          width="100%"
+          title="James Cao's Resume"
+
+        ></iframe>
+      </div>
     </div>
   );
 }
